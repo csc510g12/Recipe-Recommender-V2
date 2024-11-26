187a0de726c2862c34c63df496a4c1ef30aed4a9
YaswanthMullamuri <55339925+YaswanthMullamuri@users.noreply.github.com>
2024-11-26 12:50:27 -0500

    Refactored Test Methods

--- test/test_nav.py
+++ test/test_nav.py
@@ -1,73 +1,65 @@
 import requests;
-def test_1() :
+
+def test_find_recipe_with_egg():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=egg").text
     assert result.find("egg") != -1
 
-def test_2() :
+def test_find_indian_recipe_with_mango():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian").json()
     assert result['recipes'] != 0
 
-
-def test_3() :
-    result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian&maxTime=40").json()
-    assert result['recipes'] != 0
-    
-
-def test_4() :
+def test_find_indian_recipe_with_mango_and_max_time_40():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian&maxTime=40").json()
     assert result['recipes'] != 0
 
-
-def test_5():
+def test_find_cheese_recipe():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=Cheese").text
     assert result.find("Cheese") != -1
 
-def test_6() :
+def test_find_vegan_indian_recipe_with_mango_max_time_40():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian&maxTime=40&type=vegan").json()
     assert result['recipes'] != 0
 
-def test_7() :
+def test_find_non_vegetarian_indian_recipe_with_mango_max_time_40():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=Mango&Cuisine=Indian&maxTime=40&type=Non-Vegetarian").json()
     assert result['entries_per_page'] != 0
 
-def test_8():
+def test_find_recipe_with_invalid_cuisine():
     result = requests.get("http://localhost:1000/api/v1/recipes?Cuisine=Inidan")
     assert result is not None
 
-def test_9() :
+def test_find_vegan_recipe_with_egg_and_max_time_5():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=egg&Cuisine=Indian&maxTime=5&type=vegan").json()
     assert result is not None
 
-
-def test_10() :
+def test_find_vegetarian_recipe_with_milk_and_rice():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=milk,rice&Cuisine=Indian&maxTime=5&type=Vegetarian").text
-    assert result.find("Vegetarian")!=-1
+    assert result.find("Vegetarian") != -1
 
-def test_11() :
+def test_find_non_vegetarian_recipe_with_chicken_max_time_5():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=chicken&Cuisine=Indian&maxTime=5&type=Non-Vegetarian").text
-    assert result.find("5")!=-1
+    assert result.find("5") != -1
 
-def test_12() :
+def test_find_non_vegetarian_american_recipe_with_pork_max_time_15():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=poark&Cuisine=American&maxTime=15&type=Non-Vegetarian").json()
-    assert result['total_results'] >4
+    assert result['total_results'] > 4
 
-
-def test_13() :
+def test_find_non_vegetarian_indian_recipe_with_egg_max_time_50():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=egg&Cuisine=Indian&maxTime=50&type=Non-Vegetarian").json()
-    assert result['total_results'] !=0
+    assert result['total_results'] != 0
 
-
-def test_14():
+def test_check_recipe_status_with_fish():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=fish")
     assert result.status_code == 200
 
-def test_15():
+def test_check_recipe_status_with_fish_max_time_5():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=fish&&maxTime=5")
     assert result.status_code == 200
 
-def test_16() :
+def test_find_non_vegetarian_indian_recipe_with_chicken_max_time_5():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=chicken&Cuisine=Indian&maxTime=5&type=Non-Vegetarian").json()
     assert result is not None
-def test_17() :
+
+def test_find_recipe_with_chicken():
     result = requests.get("http://localhost:1000/api/v1/recipes?CleanedIngredients=chicken").json()
     assert result['recipes'] != 0

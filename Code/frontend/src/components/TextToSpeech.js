0e8941c07b00f5c6765f412d9f66382229a82525
azkuang2 <alexzkuang0314@gmail.com>
2025-03-24 16:48:49 -0400

    TTS work

--- Code/frontend/src/components/TextToSpeech.js
+++ Code/frontend/src/components/TextToSpeech.js
@@ -6,10 +6,39 @@ const TextToSpeech = ({ text }) => {
     const [paused, setPaused] = useState(false);
     const [speech, setSpeech] = useState(null);
 
-    
+    useEffect(() => {
+        const synth = window.speechSynthesis;
+        const speechText = new SpeechSynthesisUtterance(text);
+
+        setSpeech(speechText);
+
+        return () => {
+            synth.cancel();
+        };
+    }, [text]);
+
+    const handlePlay = () => {
+        const synth = window.speechSynthesis;
+
+        if (paused) {
+            synth.resume();
+        }
+
+        synth.speak(speech);
+
+        setPaused(false);
+    };
+
+    const handlePause = () => {
+        const synth = window.speechSynthesis;
+
+        synth.pause();
+
+        setPaused(true);
+    };
 
     return (
-        <Button> BUTTON FOR NOW </Button>
+        <Button colorPalette="green" variant="solid" onClick={ handlePlay }> BUTTON FOR NOW </Button>
     )
 }
 
